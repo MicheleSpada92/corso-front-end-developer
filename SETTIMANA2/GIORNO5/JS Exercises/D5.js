93e3a040384df11acb61ef5326784b0eae2e5c2e
Michele Spada <spadamikele@gmail.com>
2023-11-06 17:04:00 +0100

    SETTIMANA2 GIORNO5 js

--- SETTIMANA2/GIORNO5/JS Exercises/D5.js
+++ SETTIMANA2/GIORNO5/JS Exercises/D5.js
@@ -12,17 +12,37 @@ REGOLE
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
+console.log(pets)
+
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
+
+const petorder = pets.sort();
+
+
+console.log(petorder);
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
+pets.reverse();
+console.log(pets);
+
+
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+
+const index = pets.indexOf('dog'); 
+if (index !== -1) { 
+  pets.splice(index, 1); 
+  pets.push('dog'); 
+}
+console.log(pets);
+
+
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -48,15 +68,40 @@ const cars = [
   },
 ]
 
+for (let i = 0; i < cars.length; i++) {
+  cars[i].licensePlate = 'AB123CD'; 
+}
+console.log(cars);
+
+
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
 
+const newCar = {
+  brand: 'Toyota',
+  model: 'Corolla',
+  color: 'silver',
+  trims: ['le', 'se'],
+};
+cars.push(newCar); 
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop(); 
+}
+console.log(cars);
+
+
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = []
+
+for (let i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims[0]); 
+}
+console.log(justTrims);
+
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
